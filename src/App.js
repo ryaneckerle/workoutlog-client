5274426623010734934300562bed74584724082d
Ryan <ryaneckere1@gmail.com>
2021-02-03 04:23:05 -0500

    Added home component auth components and workouts components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-// import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Sitebar from './home/Navbar';
+import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 function App() {
-  return <div></div>;
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  };
+
+  const protectedViews = () => {
+    return sessionToken === localStorage.getItem('token') ? (
+      <WorkoutIndex token={sessionToken} />
+    ) : (
+      <Auth updateToken={updateToken} />
+    );
+  };
+
+  return (
+    <div>
+      <Sitebar clearToken={clearToken} />
+      {protectedViews()}
+    </div>
+  );
 }
 
 export default App;
